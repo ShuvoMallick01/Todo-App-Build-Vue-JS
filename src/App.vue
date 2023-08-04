c78920695c7ffb5e0fa961f6c99dec63ac004469
ShuvoMallick01 <shuvacse01@gmail.com>
2023-08-05 02:10:01 +0600

    First Commit todo List App

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="grid h-screen bg-gray-800 text-slate-100 place-items-center">
+    <section class="w-full max-w-md m-auto">
+      <!-- Todo Form -->
+      <form action="" class="flex items-center w-full">
+        <input
+          type="text"
+          class="w-full p-3 px-5 bg-transparent border-2 border-gray-500 rounded-l-lg outline-none"
+        />
+        <input
+          type="submit"
+          value="Add"
+          class="p-3 px-6 text-gray-800 bg-gray-300 font-medium border-gray-100 rounded-r-lg text-lg cursor-pointer"
+        />
+      </form>
+
+      <!-- FILTER ACTION -->
+      <div class=""></div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
