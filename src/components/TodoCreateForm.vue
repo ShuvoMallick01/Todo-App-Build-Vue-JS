3300bc5f63da70e1887f03feff9a3814dc930917
ShuvoMallick01 <shuvacse01@gmail.com>
2023-08-07 14:53:07 +0600

    todo Complete with split component base

---
+++ src/components/TodoCreateForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit()" class="flex items-center w-full">
+    <input
+      v-model="todoInput"
+      type="text"
+      placeholder="Input todo.."
+      class="w-full p-3 px-5 placeholder:text-slate-500 bg-transparent border-2 border-gray-500 rounded-l-lg hover:outline-none focus:border-gray-500 focus:ring-0"
+    />
+    <input
+      type="submit"
+      value="Add"
+      class="p-3 px-6 text-gray-800 bg-gray-300 font-medium border-gray-100 rounded-r-lg text-lg cursor-pointer"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoInput: "",
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      this.$emit("create", this.todoInput);
+      this.todoInput = "";
+    },
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="w-full border border-gray-600 divide-y divide-gray-600 rounded-lg">
+    <!-- Single Todo -->
+    <TodoListItem
+      v-for="todo in todoList"
+      :key="todo.id"
+      :todo="todo"
+      @complete="handleComplete($event)"
+    />
+  </ul>
+</template>
+
+<script>
+import TodoListItem from "./TodoListItem.vue";
+
+export default {
+  props: {
+    todoList: Object,
+  },
+
+  methods: {
+    handleComplete(e) {
+      this.$emit("complete", e);
+    },
+  },
+
+  components: {
+    TodoListItem,
+  },
+};
+</script>
